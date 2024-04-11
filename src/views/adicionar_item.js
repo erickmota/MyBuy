8303f679dc394ce5744bd84fe2771450ba6f1a20
Erick <erick_fcsaopaulo@hotmail.com>
2024-04-11 00:40:42 -0300

    Tela add item finalizada

--- src/views/adicionar_item.js
+++ src/views/adicionar_item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Picker } from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import IconeCorreto from "../componentes/botaoCorreto"
+import config from "../config";
 
 export default function AddItem(){
 
     const [number, onChangeNumber] = React.useState('');
+    const [selectedValue, setSelectedValue] = useState("option1");
 
     return(
 
@@ -18,10 +22,95 @@ export default function AddItem(){
             </Text>
 
             <TextInput style={styles.input}
-            onChangeText={onChangeNumber}
-            value={number}
-            keyboardType="default"
-            />
+                onChangeText={onChangeNumber}
+                value={number}
+                keyboardType="default"
+                />
+
+            <View style={styles.espacoQtd}>
+
+                <View style={{flex: 4}}>
+
+                    <Text style={styles.titulo}>
+
+                        Unidade
+
+                    </Text>
+
+                    <View style={styles.campoSelect}>
+                        <Picker
+                            selectedValue={selectedValue}
+                            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                        >
+                            <Picker.Item label="Caixa" value="option1" />
+                            <Picker.Item label="Pacote" value="option2" />
+                            <Picker.Item label="Garrafa" value="option4" />
+                            <Picker.Item label="Lata" value="option5" />
+                            <Picker.Item label="Un" value="option6" />
+                            <Picker.Item label="Kg" value="option7" />
+                            <Picker.Item label="g" value="option8" />
+                            <Picker.Item label="ml" value="option9" />
+                            <Picker.Item label="dz" value="option10" />
+                            
+                        </Picker>
+                    </View>
+
+                </View>
+
+                <View style={[styles.campoSelect, {flex: 5}]}>
+
+                    <Text style={styles.titulo}>
+
+                        Quantidade
+
+                    </Text>
+
+                    <TextInput style={styles.inputQtd}
+                        onChangeText={onChangeNumber}
+                        value={number}
+                        keyboardType="default"
+                        />
+
+                </View>
+
+            </View>
+
+            <View style={{flexDirection: "row"}}>
+
+                <View style={{flex: 5}}>
+
+                    <Text style={styles.titulo}>
+
+                        Categoria
+
+                    </Text>
+
+                    <View style={styles.campoSelect}>
+                        <Picker
+                            selectedValue={selectedValue}
+                            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                        >
+                            <Picker.Item label="Opção 1" value="option1" />
+                            <Picker.Item label="Opção 2" value="option2" />
+                            <Picker.Item label="Opção 3" value="option3" />
+                            <Picker.Item label="Opção 4" value="option4" />
+                        </Picker>
+                    </View>
+
+                </View>
+
+                <View style={{flex: 1, justifyContent: "flex-end", alignItems: "flex-end"}}>
+
+                    <Icon
+                        name="playlist-edit"
+                        size={40}
+                        color={"white"}
+                        style={styles.iconEdit}
+                        />
+
+                </View>
+
+            </View>
 
             <IconeCorreto/>
 
@@ -35,7 +124,7 @@ const styles = StyleSheet.create({
 
     container:{
 
-        flex: 1
+        flex: 1,
 
     },
 
@@ -55,6 +144,42 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginHorizontal: 15,
         color: "#777"
+
+    },
+
+    inputQtd:{
+
+        marginTop: 20,
+        fontSize: 20,
+        color: "#777"
+
+    },
+
+    espacoQtd:{
+
+        flexDirection: "row"
+
+    },
+
+    campoSelect:{
+
+        borderBottomWidth: 1,
+        borderColor: "#CCC",
+        marginHorizontal: 15
+
+    },
+
+    iconEdit:{
+
+        backgroundColor: config.cor2,
+        width:50,
+        height: 50,
+        borderRadius: 10,
+        elevation: 5,
+        marginRight: 15,
+        textAlign: "center",
+        paddingTop: 4
+
 
     }
 
