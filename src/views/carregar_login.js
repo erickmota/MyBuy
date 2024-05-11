293939acd068dde7be38a1e2f19d81c27c8d0e5b
Erick <erick_fcsaopaulo@hotmail.com>
2024-05-10 21:39:47 -0300

    erro

--- src/views/carregar_login.js
+++ src/views/carregar_login.js
@@ -1,22 +1,60 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import { Image, View, Text, StyleSheet, TextInput, useWindowDimensions, TouchableNativeFeedback, Alert } from "react-native"
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
+import * as SQLite from "expo-sqlite"
 
 import config from "../config";
 
 export default function Carregar_login({route}){
+  
+  /* Banco de dados local - Inicio da view (SQLite) */
+  const [db, setDbLocal] = useState(null);
 
+  try {
+
+    const db = SQLite.openDatabase("MyBuy.db");
+    setDbLocal(db);
+    
+  } catch (error) {
+    
+    console.error("conexão com o banco de dados local, falhou", error);
+
+  }
+
+  useEffect(()=>{
+
+    if (db) {
+      db.transaction((tx) => {
+        tx.executeSql(
+          "CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(200), token VARCHAR(200))"
+        );
+      });
+    }
+
+  },[db])
+
+  function inserirNoBancoLocal(id, nome, token){
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO usuarios (id, nome, token) VALUES ('${id}', '${nome}', '${token}')`
+      );
+    });
+
+  }
+
+  /* API */
     const navigation = useNavigation();
 
-    const {email} = route.params
-    const {senha} = route.params
+    /* const {email} = route.params
+    const {senha} = route.params */
 
     const [DATA, setData] = useState([]);
     const [view, mostrarView] = useState(false);
 
     const formData = new URLSearchParams();
-    formData.append('email', email);
-    formData.append('senha', senha);
+    formData.append('email', 'svv');
+    formData.append('senha', '11');
 
     fetch(`${config.URL_inicial_API}login`, {
     method: "POST",
@@ -29,17 +67,18 @@ export default function Carregar_login({route}){
     .then(data => {
 
         setData(data.data);
+        mostrarView(true);
 
     })
-    .catch(error => {
-    console.error('Erro ao enviar solicitação:', error);
+    .catch(errors => {
+    console.error('Erro ao enviar solicitação:', errors);
     });
 
-    setTimeout(()=>{
+    /* setTimeout(()=>{
 
         mostrarView(true)
 
-    }, 2500)
+    }, 2500) */
 
     return(
 
