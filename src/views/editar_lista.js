2a3c4cdd0e7e05de0b293f62b8d9276cb6effb27
Erick <erick_fcsaopaulo@hotmail.com>
2024-09-26 23:30:25 -0300

    Area compartilhar na lista, finalizada

--- src/views/editar_lista.js
+++ src/views/editar_lista.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext, useLayoutEffect} from 'react';
-import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert,Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useNavigation } from '@react-navigation/native';
 import { UserContext } from '../context/user';
@@ -121,17 +121,148 @@ export default function Editar_lista({route}){
 
         <View style={styles.container}>
 
-            <Text style={styles.titulo}>
+            <View>
 
-                Editar nome
+                <Text style={styles.titulo}>
 
-            </Text>
+                    Editar nome
 
-            <TextInput style={styles.input}
+                </Text>
+
+                <TextInput style={styles.input}
                 onChangeText={onChangeNumber}
                 value={number}
                 keyboardType="default"
                 />
+
+            </View>
+
+            <View style={styles.areaCompartilhar}>
+
+                <View style={styles.areaTituloCompartilhar}>
+
+                    <View style={styles.areaTxtTitulo}>
+
+                        <Text style={styles.titulo}>
+
+                            Participantes da lista
+
+                        </Text>
+
+                    </View>
+
+                    <View style={styles.areaIcon}>
+
+                        <View style={styles.BtnIconShare}>
+
+                            <Icon
+                                name="share-variant"
+                                size={17}
+                                color={"white"}
+                                style={styles.iconShare}
+                            />
+
+                        </View>
+
+                    </View>
+
+                </View>
+
+                <View style={styles.areaListaParticipantes}>
+
+                    <View style={styles.participante}>
+
+                        <View style={styles.areaImg}>
+
+                            <Image style={styles.imgUsuario} source={{ uri: `https://pessoaepessoa.com.br/wp-content/uploads/2023/04/Roberto_Pessoa.png` }} />
+
+                        </View>
+
+                        <View style={styles.areaNome}>
+
+                            <Text style={styles.nomeUsuario}>
+
+                                Erick Mota
+
+                            </Text>
+
+                        </View>
+
+                        <View style={styles.areaBtn}>
+
+                            <Text style={styles.textDono}>
+
+                                DONO
+
+                            </Text>
+
+                        </View>
+
+                    </View>
+
+                    <View style={styles.participante}>
+
+                        <View style={styles.areaImg}>
+
+                            <Image style={styles.imgUsuario} source={{ uri: `https://www.designi.com.br/images/preview/12161378.jpg` }} />
+
+                        </View>
+
+                        <View style={styles.areaNome}>
+
+                            <Text style={styles.nomeUsuario}>
+
+                                Marcelo Avelar
+
+                            </Text>
+
+                        </View>
+
+                        <View style={styles.areaBtn}>
+
+                            <Text style={styles.textRemover}>
+
+                                REMOVER
+
+                            </Text>
+
+                        </View>
+
+                    </View>
+
+                    <View style={styles.participante}>
+
+                        <View style={styles.areaImg}>
+
+                            <Image style={styles.imgUsuario} source={{ uri: `https://blog.portalpos.com.br/app/uploads/2023/08/pessoa-criteriosa.jpg` }} />
+
+                        </View>
+
+                        <View style={styles.areaNome}>
+
+                            <Text style={styles.nomeUsuario}>
+
+                                Rubens Dias
+
+                            </Text>
+
+                        </View>
+
+                        <View style={styles.areaBtn}>
+
+                            <Text style={styles.textRemover}>
+
+                                REMOVER
+
+                            </Text>
+
+                        </View>
+
+                    </View>
+
+                </View>
+
+            </View>
 
             <IconeCorreto funcao={() => salvar_nome(number)}/>
 
@@ -167,5 +298,116 @@ const styles = StyleSheet.create({
         color: "#777"
 
     },
+
+    /* Lista compartilhada */
+
+    areaTituloCompartilhar:{
+
+        flexDirection: "row"
+
+    },
+
+    areaTxtTitulo:{
+
+        flex: 4
+
+    },
+
+    areaIcon:{
+
+        flex: 1,
+        top: 30,
+        alignItems: "flex-end",
+        right: 15,
+
+    },
+
+    BtnIconShare:{
+
+        backgroundColor: config.cor2,
+        padding: 5,
+        borderRadius: 50
+
+    },
+
+    areaListaParticipantes:{
+
+        paddingHorizontal: 15,
+        marginTop: 10
+
+    },
+
+    participante:{
+
+        flexDirection: "row",
+        marginTop: 15
+
+    },
+
+    areaImg:{
+
+        flex: 1
+
+    },
+
+    areaNome:{
+
+        flex: 4,
+        alignItems: "flex-start"
+
+    },
+
+    areaBtn:{
+
+        flex: 2,
+        alignItems: "flex-end",
+
+
+    },
+
+    textDono:{
+
+        fontSize: 12,
+        top: 11,
+        color: "#AAA"
+
+    },
+
+    textRemover:{
+
+        fontSize: 12,
+        top: 11,
+        color: "red"
+
+    },
+
+    imgUsuario:{
+
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+
+    },
+
+    nomeUsuario:{
+
+        fontSize: 17,
+        top: 6,
+        color: "#666"
+
+    },
+
+    areaBtnCompartilhar:{
+
+        alignItems: "center",
+        top: 50
+
+    },
+
+    iconShare:{
+
+        right: 1
+
+    }
 
 })
