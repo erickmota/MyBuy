8f5e98b9e9ae03d46187c87e7b59ee939f1ea5ef
Erick <erick_fcsaopaulo@hotmail.com>
2024-11-26 13:17:27 -0300

    APK sendo instalado corretamente

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const config = {
+    
+    /* Estilos */
+    cor2: "#902dc4", /* Cor dos itens adicionais (cor mais fraca). */
+    cor1: "#7523a0", /* Cor principal (mais forte). */
+    corTextoSecundario: "#999", /* Cor dos textos secundários. */
+    corTelaLogin: "#e1c6f5",
+    corObs: "#ff924f", /* Cor do texto de observação dos produtos. */
+    tamanhoTextosInputs: 16, /* Tamanho padrão dos textos de títulos dos InputAccessoryView. */
+
+    /* Parâmtros */
+
+    qtd_itens_pesquisa: 5, /* Quantidade de itens retornados na pesquisa dos produtos. */
+    opacity_btn: 0.5, /* Valor padrão dos opacitys */
+
+    /* Conteúdos */
+    Foto_prod_nulo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHSFDKZsJhNfyuNuvlSdZ_fsJOelRJ4Qi6mw&s", /* Foto padrão que aparece em um produto sem foto definida. */
+    Foto_usuario_nulo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSseB6uZeeMH55OlfcMvLSB_O1j4c9eCKFcLQ&s",
+
+
+    /* Dados */
+    URL_inicial_API: "https://testes.erickmota.com/" //Web
+    /* URL_inicial_API: "http://192.168.1.8:80/API_MyBuy/" */ //Local
+
+};
+  
+export default config;
